497584e50ca1e7e9547dfe2dee8875b9dbf958e7
Eddy Martin <eddypmartin@gmail.com>
2022-06-16 10:48:45 -0400

    s5-c10 debut affichage boite modale

--- script/message.js
+++ script/message.js
@@ -1,4 +1,10 @@
 (function(){
+    let elmBoite__modale = document.createElement('div')
+    elmBoite__modale.classList.add('boite__modale')
+    let elmBody = document.querySelector('body')
+    elmBody.appendChild(elmBoite__modale);
+    
+    //document.querySelector('.boite__modale')
     let elmOK = document.querySelector('#mon_bouton')
 
     elmOK.addEventListener('mousedown', function(){
